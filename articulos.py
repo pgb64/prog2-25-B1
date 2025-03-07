04a4e5681e13aa4cd704045f0ed84a006917c91f
jorgeizquierdo-git <jib7@alu.ua.es>
2025-03-07 14:16:14 +0100

    Update articulos.py

--- articulos.py
+++ articulos.py
@@ -17,6 +17,10 @@ class Articulo: #por ahora sin herenchia
     proveedor: str
     
         Será el nombre de usuario de la persona que venda, o el cif de la empresa que los ofrezca
+
+    descripción: diccionario
+
+        En ese diccionario habrán cosas como el peso o sus dimensiones
         
     métodos
     --------------
@@ -33,21 +37,33 @@ class Articulo: #por ahora sin herenchia
         se elimina el producto indicado. Se hará automatico cuando la cantidad del producto llegue a 0
     
     '''
-    def __init__(self,nombre,cantidad,codigo,proveedor):
+    def __init__(self,nombre,cantidad,codigo,proveedor,descripcion):
         self.nombre=nombre
         self.cantidad=cantidad
-        self.codigo=codigo
-        self.proveedor=proveedor
+        self.__codigo=codigo # es necesario que sea único
+        self.proveedor=proveedor # la base de datos, por favor
+        self.descripcion=descripcion
+        # self.procedencia=procedencia, cuando tengamos la base de datos, se podrá obtener la procedencia de un paquete de alguna otra forma
+
+
+    def mostrar_codigo(self):
+        return self.__codigo
+
     
     def editar_datos(self,nombre=self.nombre,cantidad=self.cantidad,proveedor=self.proveedor):
         self.nombre=nombre
         self.cantidad=cantidad
         self.proveedor=proveedor
-        # no se eliminar objetos, aquí se comprueba
-    
-    def eliminar_producto(self): # no se como se hace esto
+        # no se eliminar objetos, aquí se comprueba.
 
-        
-    
-        
+    def eliminar_producto(self):
 
+    def pedir_paquete(self):
+        self.cantidad-=1 # quita un paquete del cantidad
+        if self.cantidad<1:
+            self.eliminar_producto()
+
+
+class Paquete(Articulo):
+
+    def __init__(self,):
