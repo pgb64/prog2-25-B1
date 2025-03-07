c8f18a14c9bce73bc8a32fc326aec02368280bcb
jorgeizquierdo-git <jib7@alu.ua.es>
2025-03-07 13:25:32 +0100

    Update articulos.py

--- articulos.py
+++ articulos.py
@@ -3,7 +3,28 @@ class Articulo: #por ahora sin herenchia
     atrubutos
     -------------
     nombre: str
+    
+        nombre del producto
+        
     cantidad :int
+    
+        cantidad total disponible del producto
+        
+    codigo: str
+    
+        los nombres pueden ser iguales, el gódigo no
+        
+    proveedor: str
+    
+        Será el nombre de usuario de la persona que venda, o el cif de la empresa que los ofrezca
+        
+    
     '''
-    def __init__(self,nombre,cantidad,):
+    def __init__(self,nombre,cantidad,codigo,proveedor):
+        self.nombre=nombre
+        self.cantidad=cantidad
+        self.codigo=codigo
+        self.proveedor=proveedor
+        
+        
 
