db7fe78a60f75ff25079c41fc408fc4ced5e2c09
jorgeizquierdo-git <jib7@alu.ua.es>
2025-03-07 13:32:57 +0100

    Update articulos.py

--- articulos.py
+++ articulos.py
@@ -18,6 +18,19 @@ class Articulo: #por ahora sin herenchia
     
         Será el nombre de usuario de la persona que venda, o el cif de la empresa que los ofrezca
         
+    métodos
+    --------------
+    __init__():
+    
+        construye un producto
+    
+    editar datos:
+    
+        cambia los datos de un producto. Por defecto no cambiará nada
+        
+    eliminar producto
+    
+        se elimina el producto indicado. Se hará automatico cuando la cantidad del producto llegue a 0
     
     '''
     def __init__(self,nombre,cantidad,codigo,proveedor):
@@ -25,6 +38,15 @@ class Articulo: #por ahora sin herenchia
         self.cantidad=cantidad
         self.codigo=codigo
         self.proveedor=proveedor
+    
+    def editar_datos(self,nombre=self.nombre,cantidad=self.cantidad,proveedor=self.proveedor):
+        self.nombre=nombre
+        self.cantidad=cantidad
+        self.proveedor=proveedor
+        # no se eliminar objetos, aquí se comprueba
+    
+    def eliminar_producto(self): # no se como se hace
         
+    
         
 
