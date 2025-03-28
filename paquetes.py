6289931342b157bcefd5866926a495b88103f745
jorgeizquierdo-git <jib7@alu.ua.es>
2025-03-28 14:41:07 +0100

    añadido atributo enviado
    enviado será True si el paquete ha sido recibido por el comprador

--- paquetes.py
+++ paquetes.py
@@ -1,6 +1,21 @@
 from articulos import Articulo
+'''
+atributos
+-------------------
+*kwargs:
 
+    debe tener los mismos atributos que artículo, pero no me importan en el código (por ahora)
+    
+enviado:
+
+    Booleano que define si un paquete ha sido enviado o no
+    
+'''
 class Paquete(Articulo):
 
     def __init__(self,*kwargs):
         super().__init()
+        self.enviado=False
+
+    def se_ha_enviado(self):
+        self.enviado=True
