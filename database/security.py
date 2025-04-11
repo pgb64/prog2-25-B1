735e9f0185c07551006bcb9c1ba454b7a72ec7bb
pgb64 <pgb64@alu.ua.es>
2025-04-11 03:11:09 +0200

    Ultima versión lista

--- database/security.py
+++ database/security.py
@@ -1,28 +1,79 @@
 import bcrypt
 
 class Security:
+    '''
+    Clase para manejar la seguridad de las contraseñas
+
+    Métodos:
+    -------
+    hash_password(password: str) -> str
+        Genera el hash de una contraseña utilizando bcrypt
+    verify_password(password: str, hash: str) -> bool
+        Verifica si la contraseña coincide con el hash proporcionado
+    check_password_strength(password: str) -> int
+        Valida la fortaleza de la contraseña
+    '''
     
     @staticmethod
     def hash_password(password):
+        '''
+        Genera el hash de una contraseña utilizando bcrypt
+        
+        Parámetros:
+        -----------
+        password : str
+            La contraseña que será cifrada
+        
+        Retorna:
+        --------
+        str
+            La contraseña cifrada como un string
+        '''
         hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         return hash.decode('utf-8')
     
     @staticmethod
     def verify_password(password, hash):
+        '''
+        Verifica si la contraseña proporcionada coincide con el hash
+        
+        Parámetros:
+        -----------
+        password : str
+            La contraseña que será verificada
+        hash : str
+            El hash de la contraseña que se desea verificar
+        
+        Retorna:
+        --------
+        bool
+            True si las contraseñas coinciden, False si no
+        '''
         return bcrypt.checkpw(password.encode(), hash.encode())
     
     @staticmethod
     def check_password_strength(password):
-        print(f"Validando fortaleza de la contraseña: {password}")  # Depuración
+        '''
+        Valida la fortaleza de la contraseña, asegurando que cumpla con ciertos requisitos:
+        - Longitud mínima de 8 caracteres
+        - Al menos una mayúscula, una minúscula, un número y un carácter especial
+        
+        Parámetros:
+        -----------
+        password : str
+            La contraseña a validar
+        
+        Retorna:
+        --------
+        int
+            200 si la contraseña es válida, 401 si no cumple con los requisitos
+        '''
         mayus = any(letter.isupper() for letter in password)
         minus = any(letter.islower() for letter in password)
         num = any(letter.isdigit() for letter in password)
         especial = any(letter in '!@#$%^&*()_+.' for letter in password)
         
-        print(f"Mayúsculas: {mayus}, Minúsculas: {minus}, Números: {num}, Especiales: {especial}")  # Depuración
         if len(password) < 8 or not (mayus and minus and num and especial):
-            print("Contraseña no cumple con los requisitos")  # Depuración
             return 401  # Contraseña inválida
         
-        print("Contraseña válida")  # Depuración
         return 200
--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+# Importamos los módulos necesarios
 import requests
 from sistema import ApiResponse
 import time
 
+
 class MenuLogin():
+    '''
+    Clase para gestionar el menú de inicio de sesión y registro
+
+    Atributos:
+    ----------
+    api : ApiResponse
+        Instancia de la clase ApiResponse para gestionar la API.
+    user : str
+        Nombre de usuario del cliente.
+    password : str
+        Contraseña del cliente.
+    token : str
+        Token de acceso del cliente.
+    path : str
+        URL base de la API.
+    '''
     path = ApiResponse.path
     def __init__(self):
         self.api = ApiResponse()
@@ -25,30 +43,37 @@ class MenuLogin():
         '''
         Lógica para iniciar sesión
         '''
+        # Solicitar usuario y contraseña
         user = input('Usuario: ')
         password = input('Contraseña: ')
+        # URL de inicio de sesión
         login_url = f'{self.path}/login?user={user}&password={password}'
 
+        # Realizar la solicitud de inicio de sesión
         try:
             response = requests.get(login_url)
 
+            # Verificar si todo funciona bien
             if response.status_code == 200:
                 data = response.json()
                 self.user = user
                 self.password = password
                 self.token = data.get('access_token')
                 return True
+            # Si no se encuentra el usuario
             elif response.status_code == 400:
                 print('Error: Usuario no encontrado.')
                 return False
+            # Si la contraseña es incorrecta
             elif response.status_code == 401:
                 print('Error: Contraseña inválida.')
                 return False
+            # Si sucede cualquier otro error
             else:
                 print(f'Error: {response.status_code}')
                 return False
-                
-        except requests.exceptions.RequestException as e:
+        # Por si sucede un error de conexión 
+        except Exception as e:
             print('Error: ', e)
             return False
 
@@ -69,7 +94,8 @@ class MenuLogin():
         print("  - Trabajador: Acceso a funciones de gestión de envíos")
         print("  - Administrador: Acceso completo a todas las funciones")
         print("===============================\n")
-        
+
+        # Pedir usuario y contraseña
         user = input('Usuario: ')
         password = input('Contraseña: ')
         
@@ -82,18 +108,20 @@ class MenuLogin():
         type_option = input("Opción: ")
         
         # Mapear opción a tipo
-        type_mapping = {
+        type_status = {
             "1": "user",
             "2": "worker", 
             "3": "admin"
         }
         
-        user_type = type_mapping.get(type_option)
+        user_type = type_status.get(type_option)
         
+        # Validar tipo de usuario
         if not user_type:
             print("Opción inválida. Se asignará 'user' por defecto.")
             user_type = "user"
 
+        # URL de registro
         signup_url = f'{self.path}/signup'
 
         data = {
@@ -102,15 +130,18 @@ class MenuLogin():
             'type': user_type
         }
 
+        # Realizar la solicitud de registro a la api
         try:
             response = requests.post(signup_url, json=data)
-
+            # Verificar si la solicitud fue exitosa
             if response.status_code == 201:
                 print('Usuario creado exitosamente.')
                 return True
+            # Si el usuario ya existe
             elif response.status_code == 409:
                 print('El usuario ya existe.')
                 return False
+            # Si la contraseña no cumple con los requisitos
             elif response.status_code == 400:
                 try:
                     error_message = response.json().get("message", "Error desconocido")
@@ -118,11 +149,12 @@ class MenuLogin():
                     error_message = "Respuesta inválida del servidor"
                 print(f"Error: {error_message}")
                 return False
+            # Si el tipo de usuario no es válido
             else:
                 print(f"Error inesperado: {response.status_code}")
                 return False
-
-        except requests.exceptions.RequestException as e:
+        # Por si sucede un error de conexión
+        except Exception as e:
             print(f"Error al conectar con el servidor: {e}")
             return False
 
@@ -156,6 +188,9 @@ class MenuLogin():
                 break
 
 class MainMenu(MenuLogin):
+    '''
+    Una vez pasado el login se pasa al menú real de la aplicación
+    '''
     def __init__(self):
         super().__init__()
     
